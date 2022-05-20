f4568a29cb260e37a56fbc657f2e8e6eeea1d5e4
ahmadradi11 <ahmadradi11@gmail.com>
2022-05-20 18:53:44 +0900

    initial commit

---
+++ lesson3_permMissingElement.js
@@ -0,0 +1,13 @@
+//Correctness 100%
+//Performance 100%
+//Detected time complexity: O(N) or O(N*log(N))
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    let n = A.length + 1
+
+    const getSum = (a, b) => a + b;
+    let sumOfA = A.reduce(getSum, 0)
+    let supposeSum = n * 0.5 * (2 + n - 1)
+    let result = supposeSum - sumOfA
+    return result
+}
